d2e964512edbad8c2094144f22a271c483aa4a3d
Ben Mohammad <benmohammad593@gmail.com>
2020-07-23 14:18:10 +0100

    Initial commit

---
+++ app/src/main/java/com/benmohammad/mvijava/TaskActivity.java
@@ -0,0 +1,14 @@
+package com.benmohammad.mvijava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tasks_act);
+    }
+}
---
+++ app/src/main/java/com/benmohammad/mvijava/tasks/ScrollChildSwipeRefreshLayout.java
@@ -0,0 +1,21 @@
+package com.benmohammad.mvijava.tasks;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+public class ScrollChildSwipeRefreshLayout extends SwipeRefreshLayout {
+
+    public ScrollChildSwipeRefreshLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public ScrollChildSwipeRefreshLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
