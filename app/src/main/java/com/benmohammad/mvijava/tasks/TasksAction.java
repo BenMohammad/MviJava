4e846868b870d93957c3c11c9afee9bcfe07d30b
Ben Mohammad <benmohammad593@gmail.com>
2020-07-24 09:54:20 +0100

    AutoValue Action added

---
+++ app/src/main/java/com/benmohammad/mvijava/tasks/TasksAction.java
@@ -0,0 +1,53 @@
+package com.benmohammad.mvijava.tasks;
+
+import androidx.annotation.Nullable;
+
+import com.benmohammad.mvijava.data.Task;
+import com.benmohammad.mvijava.mvibase.MviAction;
+import com.google.auto.value.AutoValue;
+
+public interface TasksAction extends MviAction {
+
+    @AutoValue
+    abstract class LoadTasks implements TasksAction {
+        public abstract boolean forceUpdate();
+
+        @Nullable
+        public abstract TasksFilterType filterType();
+
+        public static LoadTasks loadFilter(boolean forceUpdate, TasksFilterType filterType) {
+            return new AutoValue_TasksAction_LoadTasks(forceUpdate, filterType);
+        }
+
+        public static LoadTasks load(boolean forceUpdate) {
+            return new AutoValue_TasksAction_LoadTasks(forceUpdate, null);
+        }
+    }
+
+    @AutoValue
+    abstract class ActivateTaskAction implements TasksAction {
+        abstract Task task();
+
+        public static ActivateTaskAction create(Task task) {
+            return new AutoValue_TasksAction_ActivateTaskAction(task);
+        }
+    }
+
+    @AutoValue
+    abstract class CompleteTaskAction implements TasksAction {
+        abstract Task task();
+
+        public static CompleteTaskAction create(Task task) {
+            return new AutoValue_TasksAction_CompleteTaskAction(task);
+        }
+    }
+
+    @AutoValue
+    abstract class ClearCompletedTasksAction implements TasksAction {
+        public static ClearCompletedTasksAction create() {
+            return new AutoValue_TasksAction_ClearCompletedTasksAction();
+        }
+    }
+
+
+}
